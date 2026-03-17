ec326688486b2ef827223dfb1c7d4b2e5a86d8d7
DonPon <franzeckermann@gmail.com>
2026-03-17 21:25:32 +0100

    Changed authentication

--- eud_gui/settings.py
+++ eud_gui/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'apps.users.middleware.DevSSOMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
